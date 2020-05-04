52a802e11e571f1cd24375bbba4b6a0bb863c79a
CC <jim.lin@ikala.tv>
2020-05-04 11:45:22 +0800

    rev main.tf

--- lab-onboard-monitor/terraform/main.tf
+++ lab-onboard-monitor/terraform/main.tf
@@ -22,3 +22,28 @@ resource "google_monitoring_alert_policy" "bq_scanned_bytes_billed_warning" {
     google_monitoring_notification_channel.sre_team_channel.id
   ]
 }
+
+resource "google_monitoring_alert_policy" "vm_egress_bytes_billed_warning" {
+  display_name = "VM egress bytes billed over 512 MiB in 10 minutes"
+  combiner     = "OR"
+  conditions {
+    display_name = "VM egress bytes billed over 512 MiB in 10 minutes"
+    condition_threshold {
+      filter     = "metric.type=\"networking.googleapis.com/vm_flow/egress_bytes_count\" resource.type=\"gce_instance\""
+      duration   = "60s"
+      comparison = "COMPARISON_GT"
+      aggregations {
+        alignment_period   = "600s"
+        per_series_aligner = "ALIGN_SUM"
+      }
+      threshold_value = 536870912.0
+      trigger {
+        count = 1
+      }
+    }
+  }
+
+  notification_channels = [
+    google_monitoring_notification_channel.sre_team_channel.id
+  ]
+}
